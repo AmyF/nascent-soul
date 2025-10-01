fa94716db88d860af245666972c9b6f1196f4f3c
AmyF <840382477@qq.com>
2025-10-01 15:09:46 +0800

    remove unused SortOrder enum and sort_order variable from ZoneSortStrategy

--- addons/nascentsoul/core/sort/zone_sort_strategy.gd
+++ addons/nascentsoul/core/sort/zone_sort_strategy.gd
@@ -1,12 +1,5 @@
 extends Node
 class_name ZoneSortStrategy
 
-enum SortOrder {
-    ASCENDING,
-    DESCENDING
-}
-
-@export var sort_order: SortOrder = SortOrder.ASCENDING
-
 func sort(objs: Array) -> Array:
     return objs
